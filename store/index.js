65349e2af3312015d507fd54e7464dc5ccfdac29
Shao Eric Hu <erichu11364@gmail.com>
2018-06-29 15:49:59 -0400

    final

--- store/index.js
+++ store/index.js
@@ -1,13 +1,13 @@
-import { createStore, compose, applyMiddleware } from "redux";
-import { AsyncStorage } from "react-native";
-import thunk from "redux-thunk";
-import { persistStore, autoRehydrate } from "redux-persist";
-import { Location } from "expo";
-import reducers from "../reducers/index";
-import * as actions from "../actions/index";
+import { createStore, compose, applyMiddleware } from 'redux';
+import { AsyncStorage } from 'react-native';
+import thunk from 'redux-thunk';
+//import { persistStore, autoRehydrate } from "redux-persist";
+import { Location } from 'expo';
+import reducers from '../reducers/index';
+import * as actions from '../actions/index';
 
 const store = createStore(reducers, {}, compose(applyMiddleware(thunk)));
 
-Location.setApiKey("AIzaSyCpcxNDWenfCYl5TYwgaTc3kx0az0DH87M");
+Location.setApiKey('AIzaSyCpcxNDWenfCYl5TYwgaTc3kx0az0DH87M');
 store.dispatch(actions.checkAuth());
 export default store;
